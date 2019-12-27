4bc51c35a9a8cdffb8be8e6e7aefb4b66c257c13
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-12-27 18:55:30 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQ.Consumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://mgujdhwy:XPjqFTiaVobxvbhF6q3AIK8H4gK251Ke@spider.rmq.cloudamqp.com/mgujdhwy");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", durable: false, exclusive: false, autoDelete: false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume("hello", true, consumer);
+
+                    consumer.Received += (model, ea) =>
+                         {
+                             var message = Encoding.UTF8.GetString(ea.Body);
+
+                             Console.WriteLine("Mesaj alındı:" + message);
+                         };
+                    Console.WriteLine("Çıkış yapmak tıklayınız..");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace UdemyRabbitMQ.Publisher
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://mgujdhwy:XPjqFTiaVobxvbhF6q3AIK8H4gK251Ke@spider.rmq.cloudamqp.com/mgujdhwy");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = "Hello World";
+
+                    var bodyByte = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", routingKey: "hello", null, body: bodyByte);
+
+                    Console.WriteLine("Mesajınız gönderilmiştir.");
+                }
+
+                Console.WriteLine("Çıkış yapmak tıklayınız..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
